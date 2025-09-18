d32146098171181a26d62360cda049dda0b1f3c2
Luke Harris <6074660+luk707@users.noreply.github.com>
2025-09-18 16:27:04 +0100

    Switch to bmp280

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import time
 import socket
 
 # import board
-# from adafruit_bme280 import basic as adafruit_bme280
-from bme280 import BME280
+from bmp280 import BMP280
 from prometheus_client import start_http_server, Summary
 
 # Get the hostname of the machine
@@ -17,16 +16,15 @@ pressure = Summary("pressure", "Pressure (hPa)", ["hostname"])
 
 def main():
     # Initialize sensor
-    bme280 = BME280(0x76)
+    bmp280 = BMP280(0x76)
 
     # Start up the server to expose the metrics.
     start_http_server(80)
 
     # Continuously collect and expose sensor data
     while True:
-        temperature.labels(HOSTNAME).observe(bme280.get_temperature())
-        humidity.labels(HOSTNAME).observe(bme280.get_humidity())
-        pressure.labels(HOSTNAME).observe(bme280.get_pressure())
+        temperature.labels(HOSTNAME).observe(bmp280.get_temperature())
+        pressure.labels(HOSTNAME).observe(bmp280.get_pressure())
         time.sleep(2)
 
 
